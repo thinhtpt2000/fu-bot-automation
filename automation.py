011087c4ca79fb1d45b2ce73a1f109d7aaf18965
THINHTPT-PC\ThinhTPT <tthinh2128@gmail.com>
2021-09-11 06:26:14 +0700

    change OS

--- automation.py
+++ automation.py
@@ -22,8 +22,8 @@ def get_headless_options():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
-                                'like Gecko) Chrome/93.0.4577.63 Safari/537.36"')
+    chrome_options.add_argument('--user-agent="Mozilla/5.0 (X11; Linux x86_64; Windows NT 10.0; Win64; x64) '
+                                'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"')
     chrome_options.binary_location = os.getenv("GOOGLE_CHROME_BIN")
     return chrome_options
 
